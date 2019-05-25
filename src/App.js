c970253921c1a445e132cea7a857e9847a054581
codeoholic <mohit.tyagi@protonmail.com>
2019-05-25 12:52:18 +0530

    hola github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import arrowDown from './icons/arrowDown.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Inflation.live
+                    </p>
+                    <p className="about-project margin-top-10">I will simply display the inflation rates in various categories and states in India. This will help in visualizing the Indian economy a litte. This is a side project of <a href="https://twitter.com/codeoholic" target="_blank" rel="noopener noreferrer">@codeoholic</a>.</p>
+                    <img src={arrowDown} className="lets-start" alt="lets-start" />
+                </header>
+            </div>
+            <div className="App">
+                <header className="app-section-one">
+                    <p className="state-name">India</p>
+                    <p className="heading margin-top-20">2.92%</p>
+                    <p className="about-project margin-top-10">Average Inflation</p>
+                    <p className="sub-heading margin-top-20">1.87% - 4.23%</p>
+                    <p className="about-project margin-top-10">Rural vs Urban</p>
+                </header>
+            </div>
+            <div className="App">
+                <header className="App-header">
+                    <p className="state-name">Delhi</p>
+                    <p className="heading margin-top-20">4.24%</p>
+                    <p className="about-project margin-top-10">Average Inflation</p>
+                    <p className="sub-heading margin-top-20">11.19% - 3.87%</p>
+                    <p className="about-project margin-top-10">Rural vs Urban</p>
+                </header>
+            </div>
+        </div>
+    );
 }
 
 export default App;
