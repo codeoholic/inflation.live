2e811b6b2f1c02bcd8ae83e5e05acbdce3512fed
codeoholic <mohit.tyagi@protonmail.com>
2020-08-18 11:22:19 +0530

    adding source data; change of font; data update

--- src/App.js
+++ src/App.js
@@ -23,47 +23,47 @@ function App() {
             <div className="App">
                 <header className="app-section-one">
                     <p className="state-name">India</p>
-                    <p className="heading margin-top-20">2.92%</p>
+                    <p className="heading margin-top-20">6.93%</p>
                     <p className="about-project margin-top-10">Average Inflation</p>
-                    <p className="sub-heading margin-top-20">1.87% - 4.23%</p>
+                    <p className="sub-heading margin-top-20">6.84% - 7.04%</p>
                     <p className="about-project margin-top-10">Rural vs Urban</p>
                     <p className="sub-heading margin-top-40">inflation across categories</p>
                     <div className="margin-top-20">
                         <div className="inline">
                             <img src={food} alt="food-and-beverages" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">1.38%</p>
+                            <p className="sub-heading-large margin-top-20">8.71%</p>
                             <p className="about-project margin-top-10">Food and Beverages</p>
-                            <p className="sub-heading margin-top-10">-0.43% - 4.42%</p>
+                            <p className="sub-heading margin-top-10">8.95% - 8.26%</p>
                         </div>
                         <div className="inline">
                             <img src={tobacco} alt="tobacco-and-intoxicants" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">4.27%</p>
+                            <p className="sub-heading-large margin-top-20">12.35%</p>
                             <p className="about-project margin-top-10">Tobacco and Intoxicants</p>
-                            <p className="sub-heading margin-top-10">4.36% - 4.08%</p>
+                            <p className="sub-heading margin-top-10">11.98% - 13.22%</p>
                         </div>
                         <div className="inline">
                             <img src={clothing} alt="clothing-and-footwear" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">2.01%</p>
+                            <p className="sub-heading-large margin-top-20">2.91%</p>
                             <p className="about-project margin-top-10">Clothing and Footwear</p>
-                            <p className="sub-heading margin-top-10">0.74% - 4.26%</p>
+                            <p className="sub-heading margin-top-10">2.39% - 3.64%</p>
                         </div>
                         <div className="inline">
                             <img src={housing} alt="housing" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">4.76%</p>
+                            <p className="sub-heading-large margin-top-20">3.25%</p>
                             <p className="about-project margin-top-10">Housing</p>
-                            <p className="sub-heading margin-top-10">N.A - 4.76%</p>
+                            <p className="sub-heading margin-top-10">N.A - 3.25%</p>
                         </div>
                         <div className="inline">
                             <img src={fuel} alt="fuel-and-light" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">2.56%</p>
+                            <p className="sub-heading-large margin-top-20">2.8%</p>
                             <p className="about-project margin-top-10">Fuel and Light</p>
-                            <p className="sub-heading margin-top-10">1.95% - 3.61%</p>
+                            <p className="sub-heading margin-top-10">-0.41% - 8.9%</p>
                         </div>
                         <div className="inline">
                             <img src={miscellaneous} alt="miscellaneous" className="icon"/>
-                            <p className="sub-heading-large margin-top-20">5.10%</p>
+                            <p className="sub-heading-large margin-top-20">6.95%</p>
                             <p className="about-project margin-top-10">Miscellaneous</p>
-                            <p className="sub-heading margin-top-10">6.26% - 3.82%</p>
+                            <p className="sub-heading margin-top-10">6.1% - 7.88%</p>
                         </div>
                     </div>
                 </header>
@@ -71,9 +71,9 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <p className="state-name">Delhi</p>
-                    <p className="heading margin-top-20">4.24%</p>
+                    <p className="heading margin-top-20">3.6%</p>
                     <p className="about-project margin-top-10">Average Inflation</p>
-                    <p className="sub-heading margin-top-20">11.19% - 3.87%</p>
+                    <p className="sub-heading margin-top-20">2.11% - 3.67%</p>
                     <p className="about-project margin-top-10">Rural vs Urban</p>
                 </header>
             </div>
